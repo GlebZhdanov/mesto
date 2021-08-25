2df6ff4f95e04f83af3786fca31caa25b0e63a4c
GlebZhdanov <zhdanov-gleb@mail.ru>
2021-08-25 18:53:03 +0300

    fix:первое ревью

--- script/script.js
+++ script/script.js
@@ -2,11 +2,10 @@ let openPopupButton = document.querySelector('.profile__edit-button');
 let closePopupButton = document.querySelector('.popup__buttom-close');
 let popup = document.querySelector('.popup');
 let title = document.querySelector('.profile__title');
-let popupItem = document.querySelectorAll('.popup__item')[0];
-let popupItemSubtitle = document.querySelectorAll('.popup__item')[1];
+let popupItem = document.querySelector('.popup__item_title_active');
+let popupItemSubtitle = document.querySelector('.popup__item_subtitle_active');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 let form = document.querySelector('.popup__form')
-let button = document.querySelector('.popup__buttom')
 
 function chekPopup () {
   if (!popup.classList.contains('popup_opened')) {
@@ -20,17 +19,12 @@ function chekPopup () {
 openPopupButton.addEventListener('click', chekPopup);
 closePopupButton.addEventListener('click', chekPopup);
 
-
-form.addEventListener (
-  'submit',
+form.addEventListener ('submit',
   function(event) {
     event.preventDefault();
     title.textContent = popupItem.value;
     profileSubtitle.textContent =  popupItemSubtitle.value;
+    chekPopup ();
 }
 );
-
-button.addEventListener('click', chekPopup);
-
-
 
