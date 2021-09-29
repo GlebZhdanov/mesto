3177b9c7eaf09ff2297db08d420ad8f523e7a629
Gleb Zhdanov <zhdanov-gleb@mail.ru>
2021-09-29 10:52:49 +0300

    fix:Исправление JS

--- script/script.js
+++ script/script.js
@@ -1,47 +1,44 @@
 //Переменные на popup профиля
 
-let title = document.querySelector('.profile__title');
-let profileSubtitle = document.querySelector('.profile__subtitle');
+const title = document.querySelector('.profile__title');
+const profileSubtitle = document.querySelector('.profile__subtitle');
 
 // Переменные на действие попапов
-let popup = document.querySelector('.popup')
 
-let popupEdit = document.querySelector('.popup_edit-profile');
-let popupCard = document.querySelector('.popup_add-card');
-let popupImage = document.querySelector('.popup_image-card');
+const popupEdit = document.querySelector('.popup_edit-profile');
+const popupCard = document.querySelector('.popup_add-card');
+const popupImage = document.querySelector('.popup_image-card');
 
-let openPopupButton = document.querySelector('.profile__edit-button');
-let openPopupCardButton = document.querySelector('.profile__button');
+const openPopupButton = document.querySelector('.profile__edit-button');
+const openPopupCardButton = document.querySelector('.profile__button');
 
-let closePopupEdit = popupEdit.querySelector('.popup__buttom-close');
-let closePopupCard = popupCard.querySelector('.popup__buttom-close');
-let closePopupImage = popupImage.querySelector('.popup__buttom-close');
+const closePopupEdit = popupEdit.querySelector('.popup__buttom-close');
+const closePopupCard = popupCard.querySelector('.popup__buttom-close');
+const closePopupImage = popupImage.querySelector('.popup__buttom-close');
 
-let formEdit = popupEdit.querySelector('.popup__form');
-let formCard = popupCard.querySelector('.popup__form');
+const formEdit = popupEdit.querySelector('.popup__form');
+const formCard = popupCard.querySelector('.popup__form');
 
-let popupImagePlace = popupImage.querySelector('.popup__place')
-let popupImageOpen = popupImage.querySelector('.popup__image')
+const popupImagePlace = popupImage.querySelector('.popup__place')
+const popupImageOpen = popupImage.querySelector('.popup__image')
 
 // Инпуты двух форм
-let popupItem = document.querySelector('.popup__item_title_active');
-let popupItemSubtitle = document.querySelector('.popup__item_subtitle_active');
+const popupItem = document.querySelector('.popup__item_title_active');
+const popupItemSubtitle = document.querySelector('.popup__item_subtitle_active');
 
-let popupItemAdd = popupCard.querySelector('.popup__item_title_card');
-let popupItemUrlAdd = popupCard.querySelector('.popup__item_url_card');
+const popupItemAdd = popupCard.querySelector('.popup__item_title_card');
+const popupItemUrlAdd = popupCard.querySelector('.popup__item_url_card');
 
 
 //Реализация редактирования профиля
 chekPopup = (popup) => {
-  if (!popup.classList.contains('popup_opened')) {
-  }
     popup.classList.toggle('popup_opened');
 }
-popupItem.value = title.textContent;
-popupItemSubtitle.value = profileSubtitle.textContent;
 
 openPopupButton.addEventListener('click', () => {
   chekPopup(popupEdit)
+  popupItem.value = title.textContent;
+  popupItemSubtitle.value = profileSubtitle.textContent;
 });
 
 closePopupEdit.addEventListener('click', () => {
@@ -74,8 +71,10 @@ formEdit.addEventListener ('submit',
 formCard.addEventListener ('submit',
   function(event) {
     event.preventDefault();
-    renderCard({name: popupItemAdd.value, link: popupItemUrlAdd.value})
+    renderCard({name: popupItemAdd.value, link: popupItemUrlAdd.value});
     chekPopup (popupCard);
+    popupItemAdd.value = "";
+    popupItemUrlAdd.value = "";
 }
 );
 
@@ -114,25 +113,10 @@ function cardDelete (event) {
 
   cardTitle.textContent = data.name;
   cardImage.src = data.link;
+  cardImage.alt = data.name;
   return cardElement;
 }
 
 initialCards.forEach((data) => {
   renderCard(data)
-})
-// Реализация закрытия popup на клавишу Esc и на клик
-document.addEventListener('keydown', function (evt){
-  if (evt.key === 'Escape'){
-    popupEdit.classList.remove('popup_opened');
-    popupCard.classList.remove('popup_opened');
-    popupImage.classList.remove('popup_opened');
-  }
-})
-
-document.addEventListener('click', function(event){
-  if (event.target.classList.contains('popup')){
-    popupEdit.classList.remove('popup_opened');
-    popupCard.classList.remove('popup_opened');
-    popupImage.classList.remove('popup_opened');
-  }
 })
